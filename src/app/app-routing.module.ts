8e49f9919f6a875157588e4ea464f52253632d77
DevoRia <the.rocky.vadim@gmail.com>
2021-01-30 19:12:19 +0200

    add material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LeadPageComponent} from "./pages/lead-page/lead-page.component";
+import {LibraryPageComponent} from "./pages/library-page/library-page.component";
+import {ComparePageComponent} from "./pages/compare-page/compare-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'lead', pathMatch: 'full' },
+  { path: 'lead', component: LeadPageComponent },
+  { path: 'library', component: LibraryPageComponent },
+  { path: 'compare', component: ComparePageComponent },
+  { path: 'compare/:id', component: ComparePageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LibraryPageComponent } from './pages/library-page/library-page.component';
+import { LeadPageComponent } from './pages/lead-page/lead-page.component';
+import { ComparePageComponent } from './pages/compare-page/compare-page.component';
+import { SettingsPageComponent } from './pages/settings-page/settings-page.component';
+import { LoginPopupComponent } from './components/popups/login-popup/login-popup.component';
+import { UploadPopupComponent } from './components/popups/upload-popup/upload-popup.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LibraryPageComponent,
+    LeadPageComponent,
+    ComparePageComponent,
+    SettingsPageComponent,
+    LoginPopupComponent,
+    UploadPopupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
